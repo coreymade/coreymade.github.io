e61d6314055801191258def2d91dbdf2bb8bec30
Corey Haggard <hello@coreymade.com>
2014-03-04 00:22:57 -0700

    Site updated to 12d0245

--- javascripts/all.js
+++ javascripts/all.js
@@ -673,6 +673,49 @@
   };
 
 })(jQuery);
+(function($){
+  $(document).ready(function(){
+    var $nameField = $('#name'),
+        $emailField = $('#email')
+
+    $nameField.on('keyup', function() {
+      var $this = $(this),
+          $status = $this.parent().find('.status'),
+          val = $this.val();
+
+      if(
+        val != "" &&
+        val.length > 2 &&
+        val.length < 10
+      ) {
+        $status.addClass('flip')
+      } else {
+        $status.removeClass('flip')
+      }
+
+    })
+
+    $emailField.on('keyup', function() {
+      var $this = $(this),
+          $status = $this.parent().find('.status'),
+          val = $this.val();
+
+      if(
+        val != "" &&
+        val.length > 3 &&
+        val.length < 40 &&
+        val.indexOf('@') >= 0 &&
+        val.indexOf('@') != val.length-1
+      ) {
+        $status.addClass('flip')
+      } else {
+        $status.removeClass('flip')
+      }
+
+    })
+  })
+})(jQuery)
+;
 
 
 
