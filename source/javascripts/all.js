d640d59b1196b2e9c1d008b478f9b4f2c3d4bef9
Corey Haggard <hello@coreymade.com>
2014-03-04 22:32:39 -0700

    Styles for clients and work

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -174,3 +174,36 @@ $(function(){
   $('#container5').beforeAfter({imagePath:'/jquery/beforeafter/js/'});
   $('#container6').beforeAfter({imagePath:'/jquery/beforeafter/js/'});
 });
+
+var Menu = {
+
+  el: {
+    ham: $('.hamburger'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom')
+  },
+
+  init: function() {
+    Menu.bindUIactions();
+  },
+
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click');
+  }
+};
+
+Menu.init();
