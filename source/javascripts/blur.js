ae16e8d2f605cf5e7befb3b61b732c4016083b85
Corey Haggard <corey@gofullstack.com>
2014-10-28 11:47:43 -0400

    source

--- source/javascripts/blur.js
+++
@@ -1,5 +0,0 @@
-$(window).scroll(function() {
-  var oVal;
-  oVal = $(window).scrollTop() / 240;
-  return $(".blur").css("opacity", oVal);
-});
--- source/javascripts/fullscreen.js
+++
@@ -1,3 +0,0 @@
-$(document).ready( function(){
-  $('body').find('.header-section,.new,.blur,.case-studies').height($(window).height());
-});
