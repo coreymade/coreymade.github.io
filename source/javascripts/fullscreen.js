02386cdc0a0a336b367f14d942e68b33f4586d27
Corey Haggard <corey@gofullstack.com>
2014-10-29 18:02:49 -0400

    Adding more goodness to front header

--- source/javascripts/fullscreen.js
+++ source/javascripts/fullscreen.js
@@ -1,3 +1,8 @@
 $(document).ready( function(){
-  $('body').find('.header-section,.new,.blur').height($(window).height());
+  $('body').find('.header-section,.new,.blur,#menu').height($(window).height());
+
+    $("#toggle").click(function() {
+    $(this).toggleClass("on");
+    $("#menu").slideToggle();
+  });
 });
